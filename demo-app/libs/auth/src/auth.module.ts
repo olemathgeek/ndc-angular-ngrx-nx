3728087fda3a7cf2a66c9cb7c3bd9227d6af22cb
olemathgeek <e.kristense@gmail.com>
2018-05-07 16:01:15 -0500

    more code

--- demo-app/libs/auth/src/auth.module.ts
+++ demo-app/libs/auth/src/auth.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './containers/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth/auth.service';
+import { MaterialModule } from '@demo-app/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const authRoutes: Route[] = [{ path: 'login', component: LoginComponent }];
 const COMPONENTS = [LoginComponent, LoginFormComponent];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, MaterialModule, ReactiveFormsModule],
   declarations: [COMPONENTS],
   exports: [COMPONENTS],
   providers: [AuthService]
