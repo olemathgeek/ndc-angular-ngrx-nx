740fb2ee6e9dc5824e8118717c93f624d0fc96e8
olemathgeek <e.kristense@gmail.com>
2018-05-07 16:18:30 -0500

    more code

--- demo-app/libs/auth/src/services/auth/auth.service.ts
+++ demo-app/libs/auth/src/services/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Authenticate } from '@demo-app/data-models';
+import { Authenticate, User } from '@demo-app/data-models';
 import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  login(authenticate: Authenticate): Observable<any>{
-    return this.httpClient.post('http://localhost:3000/login', authenticate);
+  login(authenticate: Authenticate): Observable<User> {
+    return this.httpClient.post<User>(
+      'http://localhost:3000/login',
+      authenticate
+    );
   }
-
 }
