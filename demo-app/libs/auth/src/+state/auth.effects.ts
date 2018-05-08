45f66514c17265aab5406e8ff3180a0f5667cb04
olemathgeek <e.kristense@gmail.com>
2018-05-08 13:16:59 -0500

    more code

--- demo-app/libs/auth/src/+state/auth.effects.ts
+++ demo-app/libs/auth/src/+state/auth.effects.ts
@@ -5,24 +5,39 @@ import { AuthState } from './auth.reducer';
 import { DataPersistence } from '@nrwl/nx';
 import { AuthActionTypes } from './auth.actions';
 import { AuthService } from '@demo-app/auth/src/services/auth/auth.service';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, mergeMap, catchError } from 'rxjs/operators';
 import { User } from '@demo-app/data-models';
 import { Router } from '@angular/router';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class AuthEffects {
 
-  @Effect()
-  loadAuth$ = this.dataPersistence.fetch(AuthActionTypes.Login, {
-    run: (action: authActions.LoginAction) => {
-      return this.authService.login(action.payload)
-      .pipe(map((user: User) => (new authActions.LoginSuccessAction(user))))
-    },
+  // @Effect()
+  // loadAuth$ = this.dataPersistence.fetch(AuthActionTypes.Login, {
+  //   run: (action: authActions.LoginAction) => {
+  //     return this.authService.login(action.payload).pipe(map((user: User) => (new authActions.LoginSuccessAction(user))))
+  //   },
 
-    onError: (action: authActions.LoginAction, error) => {
-      return new authActions.LoginFailAction(error);
-    }
-  });
+  //   onError: (action: authActions.LoginAction, error) => {
+  //     return new authActions.LoginFailAction(error);
+  //   }
+  // });
+
+  @Effect()
+  login = this.actions$
+    .ofType(AuthActionTypes.Login)
+    .pipe(
+      mergeMap((action: authActions.LoginAction) =>
+        this.authService
+          .login(action.payload)
+          .pipe(
+            map((user: User) => new authActions.LoginSuccessAction(user)),
+            catchError(error => of(new authActions.LoginFailAction(error)))
+          )
+      )
+    );
+
 
   @Effect({dispatch: false})
   navigateToProfile$ = this.actions$.pipe(
