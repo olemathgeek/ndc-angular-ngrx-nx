8599c88f8a8f8ac12d084a55e231f2dc88ea40e6
olemathgeek <e.kristense@gmail.com>
2018-05-08 16:07:54 -0500

    more code

--- demo-app/libs/admin-portal/users/src/containers/user-list/user-list.component.ts
+++ demo-app/libs/admin-portal/users/src/containers/user-list/user-list.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from '@demo-app/data-models';
+import { UsersState } from './../../+state/users.reducer';
+import * as usersActions from './../../+state/users.actions'
+import { selectAllUsers } from './../../+state/index'
 
 @Component({
-  selector: 'user-list',
+  selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  constructor() {}
+  users$: Store<User[]>;
 
-  ngOnInit() {}
+  constructor(private store: Store<UsersState>) {}
+
+  ngOnInit() {
+    this.store.  dispatch(new usersActions.LoadUsersAction());
+    this.users$ = this.store.select(selectAllUsers);
+  }
 }
