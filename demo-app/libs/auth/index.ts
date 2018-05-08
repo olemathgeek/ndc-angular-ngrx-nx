24c6cb18228ae530f286bf32c02940b816ba3f9f
olemathgeek <e.kristense@gmail.com>
2018-05-08 14:57:47 -0500

    more code

--- demo-app/libs/auth/index.ts
+++ demo-app/libs/auth/index.ts
@@ -1,3 +1,4 @@
+export { AuthAdminGuard } from './src/guards/auth-admin/auth-admin.guard';
 export { AuthGuard } from './src/guards/auth/auth.guard';
 export { AuthModule, authRoutes } from './src/auth.module';
 export { AuthState } from './src/+state/auth.reducer';
